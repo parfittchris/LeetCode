a1a9b6c5de1bd7452d15251f8fde880bebc7dd1e
Chris Parfitt <parfitt.chris7@gmail.com>
2019-10-14 17:17:34 -0400

    add array 33

---
+++ Arrays/33_Search_in_rotated_sorted_array.js
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e., [0, 1, 2, 4, 5, 6, 7] might become[4, 5, 6, 7, 0, 1, 2]).
+
+// You are given a target value to search.If found in the array return its index, otherwise return -1.
+
+// You may assume no duplicate exists in the array.
+
+// Your algorithm's runtime complexity must be in the order of O(log n).
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    if (nums.length === 0) return -1;
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] === target) return mid;
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1
+};
