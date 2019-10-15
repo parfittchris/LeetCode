aa9c5da17c2329bc50aab1baa62181cdf8bec036
Chris Parfitt <parfitt.chris7@gmail.com>
2019-10-15 16:35:15 -0400

    add Graphs folder

---
+++ Graphs/133_Clone_Graph.js
@@ -0,0 +1,32 @@
+
+//https://leetcode.com/problems/clone-graph/
+//Given a reference of a node in a connected undirected graph, return a deep copy(clone) of the graph.Each node in the graph contains a val(int) and a list(List[Node]) of its neighbors.
+
+/**
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {
+    let visited = {};
+    return traverse(node)
+
+    function traverse(node) {
+        if (!node) return node;
+
+        if (!visited[node.val]) {
+            visited[node.val] = new Node(node.val)
+            visited[node.val].neighbors = node.neighbors.map(neighbor => traverse(neighbor))
+        }
+
+        return visited[node.val]
+    }
+
+
+};
